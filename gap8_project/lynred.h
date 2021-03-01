8f9a4e57e7e4313c29d7223bc522943aab1d69e0
Marco F <marco.fariselli@greenwaves-technologies.com>
2021-03-01 08:55:57 +0100

    Ported names to nntool 3.10

--- gap8_project/lynred.h
+++ gap8_project/lynred.h
@@ -11,13 +11,13 @@ extern AT_HYPERFLASH_FS_EXT_ADDR_TYPE lynred_L3_Flash;
 #endif
 #define INPUT1_Q S0_Op_input_1_Q
 
-#define OUTPUT1_Q S4_Op_output_1_Q
-#define OUTPUT2_Q S10_Op_output_2_Q
-#define OUTPUT3_Q S16_Op_output_3_Q
-#define OUTPUT4_Q S22_Op_output_4_Q
-#define OUTPUT5_Q S6_Op_output_5_Q
-#define OUTPUT6_Q S12_Op_output_6_Q
-#define OUTPUT7_Q S18_Op_output_7_Q
-#define OUTPUT8_Q S24_Op_output_8_Q
+#define OUTPUT1_Q S11_Op_output_1_Q
+#define OUTPUT2_Q S23_Op_output_2_Q
+#define OUTPUT3_Q S35_Op_output_3_Q
+#define OUTPUT4_Q S46_Op_output_4_Q
+#define OUTPUT5_Q S15_Op_output_5_Q
+#define OUTPUT6_Q S27_Op_output_6_Q
+#define OUTPUT7_Q S39_Op_output_7_Q
+#define OUTPUT8_Q S50_Op_output_8_Q
 
 #endif //__LYNRED_H__
